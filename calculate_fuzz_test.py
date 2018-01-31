a5a89b035bc87b71dbd7b871904c8c37ac1d1bdd
lekapinun <lekapinun@gmail.com>
2018-02-01 04:30:15 +0700

    add defuzz but not yet

--- calculate_fuzz_test.py
+++ calculate_fuzz_test.py
@@ -1,5 +1,8 @@
 import numpy as np
 from membership_test import firstMem, lastMem, triangle
+import plotly
+from plotly.graph_objs import Scatter, Layout
+import plotly.graph_objs as go
 
 def fuzzification(input_feature, input_feature_top, input_feature_low, numOfFeature, n_rule) :
     out_low = [] 
@@ -55,3 +58,92 @@ def interface(out_low, out_top, rule_feature) :
             result_rule.append(rule_feature[i][len(rule_feature[3])-1])
         i+=1
     return result_top, result_low, result_rule
+
+def outputset(result_top, result_low, result_rule, n_class) :
+    out = []
+    for i in range(0, n_class) :
+        out.append( [ [0, 1], [0, 1]])
+    for i in range(0,len(result_top)) :
+        # print('----')
+        # print(result_top[i])
+        # print(result_low[i])
+        # print(result_rule[i])
+        # print('yes')
+        # print(result_rule[i])
+        # print('no')
+        for j in range(0, n_class):
+            if j != result_rule[i] :
+                # print(j)
+                if result_top[i] > out[j][1][0] :
+                    out[j][1][0] = result_top[i]
+                if result_low[i] < out[j][1][1] :
+                    out[j][1][1] = result_low[i]
+
+            else :
+                if result_top[i] > out[j][0][0] :
+                    out[j][0][0] = result_top[i]
+                if result_low[i] < out[j][0][1] :
+                    out[j][0][1] = result_low[i]
+    for i in range(0, n_class) :
+        if out[i][0] == [0, 1] :
+            out[i][0] = [0, 0]
+    # print(' ')
+    # for x in out :
+    #     print(x)
+    return out
+
+def defuzzy( output_feature_top, output_feature_low, outputs ) :
+    x_axis = np.linspace(0,4,1001)
+    y_axis = []
+    for i in range(0, len(outputs)) :
+        y_axis.append( [ [], [] ] )
+    i = 0
+    for output in outputs :
+        yes = output[0]
+        no = output[1]
+        print('-----')
+        print(yes)
+        print(no)
+        for x in x_axis :
+            y_temp_top_yes = 0
+            y_temp_top_no = 0
+            y_temp_low_yes = 0
+            y_temp_low_yes = 0
+
+            y_temp_top_yes = triangle(output_feature_top[1] , x)
+            if y_temp_top_yes > yes[0] :
+                y_temp_top_yes = yes[0]
+
+            y_temp_top_no = triangle(output_feature_top[0] , x)
+            if y_temp_top_no > no[0] :
+                y_temp_top_no = no[0]            
+
+            y_temp_low_yes = triangle(output_feature_low[1] , x)
+            if y_temp_low_yes > yes[1] :
+                y_temp_low_yes = yes[1]
+
+            y_temp_low_no = triangle(output_feature_low[0] , x)
+            if y_temp_low_no > no[1] :
+                y_temp_low_no = no[1]
+
+            y_axis[i][0].append(np.max([ y_temp_top_yes , y_temp_top_no]))
+            y_axis[i][1].append(np.max([ y_temp_low_yes , y_temp_low_no]))
+        i = i + 1  
+    # print(y_axis[0])  
+    x = x_axis
+    y1 = y_axis[0][0]
+    y2 = y_axis[0][1]
+    trace1 = Scatter(
+                x = x,
+                y = y1,
+            )
+    trace2 = Scatter(
+                x = x,
+                y = y2,
+            )
+    data2 = [ trace1, trace2 ]
+
+    fig = go.Figure(data = data2)
+
+    plotly.offline.plot(fig, filename='output.html')
+    return 1
--- fuzzi.py
+++ fuzzi.py
@@ -3,13 +3,18 @@ import numpy as np
 # import matplotlib.pyplot as plt
 from membership_test import getMF, getOutput, firstMem, lastMem, triangle
 from rule_test import getRule
-from calculate_fuzz_test import fuzzification, interface
+from calculate_fuzz_test import fuzzification, interface, outputset, defuzzy
 
 input_feature_top, input_feature_low, numOfFeature, n_rule = getMF()
+output_feature_top, output_feature_low = getOutput()
 rule_feature, numOfRule = getRule()
+n_class = 5
 
 input_feature=[0.711538461538462, 0.903846153846154, 0, 0.100000000000000]
 out_low, out_top = fuzzification(input_feature, input_feature_top, input_feature_low, numOfFeature, n_rule)
 
 result_top, result_low, result_rule = interface(out_low, out_top, rule_feature)
 
+outputs = outputset(result_top, result_low, result_rule, n_class)
+
+defuzzy(output_feature_top, output_feature_low, outputs)
