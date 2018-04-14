b7aa254d17f31ce1333d27b25920942daf9704b1
lekapinun <lekapinun@gmail.com>
2018-04-15 00:11:22 +0700

    confuse between X and Y

--- main.py
+++ main.py
@@ -16,40 +16,74 @@ print(targetIMG)
 space = 10
 img = cv.imread(targetIMG,0) 
 imgRGB = cv.imread(targetIMG,1) 
-imgs = sliceIMG(img, 130, space)
-len_i = np.size(imgs,0)
-len_j = np.size(imgs,1)
+# imgs = sliceIMG(img, 130, space)
+# len_i = np.size(imgs,0)
+# len_j = np.size(imgs,1)
 
 input_feature_top, input_feature_low, numOfFeature, n_rule = getMF_WSD()
 output_feature_top, output_feature_low = getOutput()
 rule_feature, numOfRule = getRule()
-n_class = 5
+n_class = 2
 
-for i in range(0,len_i) :
-    for j in range(0,len_j) :
-        # print(imgs[i][j])
-        print( (i * len_j ) + j )
-        # print(resultFeatures(imgs[i][j]))
-        xxxxxxxx = str(resultFeatures(imgs[i][j]))
-        input_fuzzy = resultFeatures(imgs[i][j])
+height, width = img.shape
+index_Y = 0
+black = np.zeros( (2048,2048), np.uint8 )
+
+
+size_windown = 130
+while index_Y + size_windown <= height :
+    index_X = 0
+    while index_X + size_windown <= width :
+        target = img[index_Y: index_Y + 130, index_X: index_X + 130]
+        input_fuzzy = resultFeatures(target)
+        xxxxxxxx = str(input_fuzzy)
         if ~np.isnan(np.sum(input_fuzzy)) :
-            ### Fuzzification
+            ## Fuzzification
             out_low, out_top = fuzzification(input_fuzzy, input_feature_top, input_feature_low, numOfFeature, n_rule)
             ### Inference
             result_top, result_low, result_rule = inference(out_low, out_top, rule_feature)
             ### Output
             outputs = outputEachRule (result_top, result_low, result_rule, output_feature_top, output_feature_low, n_class)
             result = defuzzy(outputs)
-            # print(result)
-            xxxxxxxx = xxxxxxxx + ' ----> ' +str(result)
-            # Detect
-            if result[1] > result[0] and result[0] > 1.4  :
-                cv.rectangle(imgRGB, (j*space,i*space), ((j*space)+130,(i*space)+130), (0,0,255), 2)
-                print(xxxxxxxx)
+            if result[1] > 1.9 and result[0] < 1.1  :
+                xxxxxxxx = xxxxxxxx + ' ----> ' +str(result)
+                black[index_Y: index_Y + 130, index_X: index_X + 130] = img[index_Y: index_Y + 130, index_X: index_X + 130]
+                cv.rectangle(imgRGB, (index_X,index_Y), ((index_X)+130,(index_Y)+130), (0,0,255), 2)
+                print(  str(index_X) + ',' + str(index_Y) + ' ' + str(xxxxxxxx))
+
+        index_X = index_X + space
+        # print(index_X, index_Y)
+    index_Y = index_Y + space
+
+# for i in range(0,len_i) :
+#     for j in range(0,len_j) :
+#         # print(imgs[i][j])
+#         # print( (i * len_j ) + j )
+#         # print(resultFeatures(imgs[i][j]))
+#         xxxxxxxx = str(resultFeatures(imgs[i][j]))
+#         input_fuzzy = resultFeatures(imgs[i][j])
+#         if ~np.isnan(np.sum(input_fuzzy)) :
+#             ### Fuzzification
+#             out_low, out_top = fuzzification(input_fuzzy, input_feature_top, input_feature_low, numOfFeature, n_rule)
+#             ### Inference
+#             result_top, result_low, result_rule = inference(out_low, out_top, rule_feature)
+#             ### Output
+#             outputs = outputEachRule (result_top, result_low, result_rule, output_feature_top, output_feature_low, n_class)
+#             result = defuzzy(outputs)
+#             # print(result)
+#             xxxxxxxx = xxxxxxxx + ' ----> ' +str(result)
+#             # Detect
+#             if result[1] > 1.9 and result[0] < 1.1  :
+#                 print( (i * len_j ) + j )
+#                 cv.rectangle(imgRGB, (j*space,i*space), ((j*space)+130,(i*space)+130), (0,0,255), 2)
+#                 cv.rectangle(imgRGB, (i*space,j*space), ((i*space)+130,(j*space)+130), (255,0,0), 2)
+#                 black[]
+#                 print(xxxxxxxx)
 
 
 cv.imshow('img',img)
-cv.imshow('xxxx',imgRGB)
+cv.imshow('black',black)
+cv.imshow('imgRGB',imgRGB)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
